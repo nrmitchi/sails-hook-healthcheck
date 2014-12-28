8336e4a62fc4076c5728b3d46640fc6ab85b99d5
Nick Mitchinson <nrmitchi@gmail.com>
2014-12-28 14:57:12 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * sails-hook-healthcheck hook
+ */
+
+module.exports = function (sails) {
+  return {
+
+    // Run when sails loads-- be sure and call `next()`.
+    initialize: function (next) {
+      return next();
+    },
+
+    routes: {
+
+      '/_healthcheck': function returnTime(req, res, next) {
+        return res.end(new Date().getTime().toString());
+      },
+      '/_healthcheck/ping': function returnPong(req, res, next) {
+        return res.end('pong');
+      }
+
+    }
+
+  };
+};
